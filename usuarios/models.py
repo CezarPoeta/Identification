d00a86c2dc7d013208644175160a26058a9b07e3
Cezar Roberto Poeta <cezarrobertopoeta@gmail.com>
2021-06-01 18:02:34 -0300

    Update models.py
    Update models

--- usuarios/models.py
+++ usuarios/models.py
@@ -15,12 +15,12 @@ class UsuarioManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_user(self, email, password='Nome', **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
        # extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(email, password, **extra_fields)
 
-    def create_superuser(self, email, password='Nome', **extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_staff', True)
 
